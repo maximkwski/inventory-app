1077a2a2e1405ca6edc462563697356d644e55a8
Maksym <makkie.makowski@gmail.com>
2025-10-14 13:06:16 -0400

    add views

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const itemRoutes = require("./routes/itemRoutes");
 const PORT = process.env.PORT || 8000;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
